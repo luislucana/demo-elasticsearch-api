a4e9c3227b377e87619b2d4dda4ea00aa9f0cd86
Luis Lucana <luislucana@gmail.com>
2020-01-22 00:11:07 -0300

    Criação dos arquivos iniciais.

---
+++ src/main/java/br/com/exemplo/demoelasticsearchapi/DemoElasticsearchApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.demoelasticsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoElasticsearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoElasticsearchApiApplication.class, args);
+	}
+
+}
